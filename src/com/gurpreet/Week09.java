d2b29522177b0e1d458748865f73309fb38d3000
Gurpreet Aulakh <gurpreetaulakh2992@gmail.com>
2021-03-20 09:04:46 -0400

    create project

---
+++ src/com/gurpreet/Week09.java
@@ -0,0 +1,11 @@
+package com.gurpreet;
+
+public class Week09 {
+    public static void main(String[] args) {
+        String str="Welcome";
+        System.out.println("Length" +str.length());
+
+        str.concat("to week09 class");
+        System.out.println(str);
+    }
+}
