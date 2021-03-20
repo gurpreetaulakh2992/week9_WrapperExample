c437b04a407967def7c623fd4c514e0d323e8ba4
Gurpreet Aulakh <gurpreetaulakh2992@gmail.com>
2021-03-20 09:08:05 -0400

    method operation on string

--- src/com/gurpreet/Week09.java
+++ src/com/gurpreet/Week09.java
@@ -1,11 +1,15 @@
 package com.gurpreet;
 
+import java.util.Locale;
+
 public class Week09 {
     public static void main(String[] args) {
         String str="Welcome";
         System.out.println("Length" +str.length());
 
-        str.concat("to week09 class");
+        str.concat("to week09 class"); //not mutuable
+        str=str.concat("to the week09 class"); //this will concat
+        str=str.toUpperCase();
         System.out.println(str);
     }
 }
