b539e6eb0d68323cf839e06d4d1082b8df080771
Gurpreet Aulakh <gurpreetaulakh2992@gmail.com>
2021-03-20 09:24:58 -0400

    operations such as substring,contains,indexOf

--- src/com/gurpreet/Week09.java
+++ src/com/gurpreet/Week09.java
@@ -4,12 +4,26 @@ import java.util.Locale;
 
 public class Week09 {
     public static void main(String[] args) {
-        String str="Welcome";
-        System.out.println("Length" +str.length());
-
-        str.concat("to week09 class"); //not mutuable
-        str=str.concat("to the week09 class"); //this will concat
-        str=str.toUpperCase();
+        String str="Welcome to Lambton College";
+        System.out.println("Length: " +str.length());
+        str=str.concat("Toronto"); //not mutuable
+//        str=str.concat("to the week09 class"); //this will concat
+//        str=str.toUpperCase();
         System.out.println(str);
+        //Extract the word week09
+        String find="Lambton";
+        int indexOfFindString=str.indexOf(find);
+        String str1;
+        str1 = str.substring(indexOfFindString,indexOfFindString+7);
+        System.out.println(str1);
+        //check the word college exist or not
+        String wordToFind="College";
+        System.out.println(str.contains(wordToFind));
+        if(str.indexOf(wordToFind)>=0){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
     }
 }
